808bb8f70884516f2b9269c273d314577b951d53
jcorozco81 <jcorozco@gmail.com>
2025-03-01 20:00:52 -0500

    Fixed path

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -55,10 +55,10 @@ fi
 for i in $( seq 1 $NUMFILES)
 do
 	# ./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
-		usr/bin/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+		/usr/bin/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
